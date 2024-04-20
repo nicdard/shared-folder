3cc83ea89750423e4a23b9617eacd72ea488d06e
Nicola Dardanis <ndardanis@student.ethz.ch>
2024-04-20 17:31:09 +0200

    refactor: apply license

--- services/mysql/pki_database.sql
+++ services/mysql/pki_database.sql
@@ -1,16 +1,14 @@
-# Copyright (C) 2024 Nicola Dardanis <nicdard@gmail.com>
-#
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, version 3.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program. If not, see <https://www.gnu.org/licenses/>.
-#
+-- Copyright (C) 2024 Nicola Dardanis <nicdard@gmail.com>
+--
+-- This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+-- License as published by the Free Software Foundation, version 3.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+-- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along with this program. If not, see <https://
+-- www.gnu.org/licenses/>.
+--
 /* 
 The table where to store the certificates:
 - serial_number: the Serial Number: https://www.rfc-editor.org/rfc/rfc5280#section-4.1.2.2
