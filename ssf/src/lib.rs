c883d96f8e9b75c89b778e2383539bcda113f369
Nicola Dardanis <ndardanis@student.ethz.ch>
2024-04-23 10:49:02 +0200

    feat: add all subcrates

---
+++ ssf/src/lib.rs
@@ -0,0 +1,37 @@
+// Copyright (C) 2024 Nicola Dardanis <nicdard@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+//
+use cfg_if::cfg_if;
+use utils::set_panic_hook;
+use wasm_bindgen::prelude::*;
+
+mod utils;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    set_panic_hook();
+    alert(&format!("Hello, {}!", name));
+}
+
+// Less efficient allocator than the default one which however is super small, only 1K in code size (compared to ~10K)
+cfg_if! {
+    if #[cfg(feature = "wee_alloc")] {
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
